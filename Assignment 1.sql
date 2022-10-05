3790c38177f35b0117ca8cb2fb2f910759afb59b
Nickbrick1 <114082540+Nickbrick1@users.noreply.github.com>
2022-10-05 16:06:58 +0200

    Add files via upload
    Hier staat 16 en 18 bij, check redenatie even bij 18

--- Assignment 1.sql
+++ Assignment 1.sql
@@ -119,3 +119,131 @@ from xemp as a
 join xemp as b on a.bossno = b.empno
 where a.deptname = 'Accounting' and a.empsalary > 20000
 
+--Q7
+select b.splname, count(distinct a.itemname) as total
+from xdel as a
+join xspl as b on a.splno = b.splno
+group by b.splname
+having count(distinct a.itemname) >= 5
+order by b.splname;
+
+--Q8
+select a.deptname, count(b.itemname) 
+from xdept as a
+join xsale as b on b.deptname = a.deptname
+where a.deptfloor in (1,2)
+group by a.deptname
+having count(b.itemname) >= 5
+
+--Q9
+select a.empfname as manager, count(b.empfname) as direct_employees
+into #direct
+from xemp as a
+join xemp as b on a.empno = b.bossno
+group by a.empfname
+
+select *
+from #direct 
+where #direct.direct_employees = (select Min(#direct.direct_employees)
+								  from #direct)
+drop table #direct
+
+--Q10
+drop table if  exists #salary
+select *
+into #salary
+from xemp as a
+where a.deptname != 'Clothes' and empsalary > (select min(empsalary)
+											   from xemp as b
+											   where b.deptname = 'Clothes');
+
+drop table if  exists #salary2
+select *, dense_rank() over(order by a.empsalary desc) as ranking
+into #salary2
+from #salary as a
+where a.deptname != 'Clothes' and empsalary > (select min(empsalary)
+											   from xemp as b
+											   where b.deptname = 'Clothes');
+
+select top(3) *
+from #salary2
+
+drop table #salary
+drop table #salary2
+
+--11
+select a.deptname
+from xemp as a
+where a.deptname in (select b.deptname
+					 from xsale as b
+					 where b.itemname in ('compass', 'elephant polo stick'))
+group by a.deptname
+having avg(empsalary) > 10000
+
+select a.deptname
+from xemp as a
+group by a.deptname
+having avg(empsalary) > 10000
+intersect
+select b.deptname
+from xsale as b
+where b.itemname in ('compass', 'elephant polo stick')
+
+--Q12
+select *
+from xspl as a
+where a.splno in (select b.splno
+				  from xdel as b
+				  where b.itemname != 'stetson')
+
+select distinct a.splno, a.splname
+from xspl as a
+join xdel as b on a.splno = b.splno
+where b.itemname != 'stetson'
+
+--Q13
+select *, rtrim(left(itemname, charindex(' ', itemname))) as itemname_firstword
+from xitem
+-- nu nog een functie dr van maken
+
+--Q16
+select distinct itemname
+from xdel
+where splno = (select splno
+			   from xspl
+			   where splname = 'Nepalese Corp.')
+except
+select itemname
+from xsale
+where deptname = 'Clothes'
+
+----------
+select distinct itemname
+from xdel
+where splno = (select splno
+			   from xspl
+			   where splname = 'Nepalese Corp.') and itemname not in (select itemname
+																	  from xsale
+																	  where deptname = 'Clothes')
+
+--Q18
+drop table if exists #cartesian_temp
+select sales.saleno,sales.saleqty,sales.itemname as sale_itemname,sales.deptname, items.itemname as item_itemname,items.itemtype,items.itemcolor
+into #cartesian_temp
+from xsale as sales ,xitem as items;
+
+--Cartesian product so all records are unique
+drop table if exists #unique_records
+select *
+into #unique_records
+from #cartesian_temp
+
+-- As unique records has the same amount of rows as the cartesian product we know that there are no duplicates
+delete
+from #cartesian_temp
+
+drop table #cartesian_temp
+drop table #unique_records
+
+
+
