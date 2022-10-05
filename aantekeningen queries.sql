c16b2465cc09dcc49a206e4126373d91c2f894e6
DicodeGier <114080631+DicodeGier@users.noreply.github.com>
2022-10-05 15:05:53 +0200

    Add files via upload

---
+++ aantekeningen queries.sql
@@ -0,0 +1,18 @@
+/*differences: INDIEN NODIG, INTERPRETATIE AANGEVEN, IS NOODZAKELIJK VOOR NAKIJKEN
+			   2 (Daan had vloer = 1)
+			   4 verschillende antwoorden, hier nog goed naar kijken
+			   6ab Dico/Nick beter
+			   6c Daan/Nick ander antwoord, hier nog goed naar kijken 
+			   7 nog niet vergeleken
+			   8 controleren, interpretatie aangeven
+			   9a nog niet vergeleken, 9b comment maken
+			   12b nog niet vergeleken
+			   14 nog niet vergeleken
+			   15 interpretatie, spnlo = 102 misschien anders
+			   16 interpretatie, 16b dico nog niet vergeleken
+			   17 zelfde als 15
+			   18b Daan fout, nog geen antwoord
+			   19 nog niet vergeleken
+			   20bd nog niet vergeleken
+			   22 nog niet vergeleken (zal denk ik wel kloppen)
+*/
