8fd77c4ec63747e4c3349fe5ea36a9400ab31d67
DicodeGier <114080631+DicodeGier@users.noreply.github.com>
2022-10-09 14:56:58 +0200

    Update aantekeningen queries.sql

--- aantekeningen queries.sql
+++ aantekeningen queries.sql
@@ -1,15 +1,2 @@
 --geupdate lijst met aantekeningen:
-2c -- Turnhout antwoord klopt niet, Nick denk ik wel
-4 -- a.deptname is geen foreign key om op te joinen
-7 -- verschillende antwoorden, zit hem in distinct
-8 -- verschillende queries met verschillende antwoorden
-9a -- 0 values weghalen want wordt gevraagd naar managers
-9b -- query van Nick compacter en geeft #employees
-10a -- geen ordering van laag naar hoog
-10b -- Turnhout verkeerd om geordend, Nick geen ordering
-10c -- Turnhout niet de top 3 hoogste salarissen
-12 -- Turnhout queries komen niet op zelfde antwoord, Nick wel
-14 -- niet kunnen vergelijken
-15 & 17-- interpretatie geven
-20b & d -- niet kunnen vergelijken
-20d -- waarvoor is laatste select
+14 en 20bd nog niet kunnen vergelijken
