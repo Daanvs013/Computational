22e1c7354be0ff815cf7a649b1e0a5c365b5ee99
DicodeGier <114080631+DicodeGier@users.noreply.github.com>
2022-11-02 14:30:11 +0100

    Add files via upload

--- functions.py
+++ functions.py
@@ -18,33 +18,37 @@ def cleanString(input):
         pos = re.search('\s\s',output)
     return output
 
-def getPages(input):
-    ## hoe verbeteren: pagina in anderet talen zoeken of afkorting van pages, meer cases toevoegen
-
-    pos = re.search('(?i)pages|bladzijde|seite|page|pp.', input) ## returns tuple with start and end position of pages in the string 
-    if (pos == None):
-        return None
-    else:
-        pos = pos.span() 
-        page_numbers = input[pos[1]+1:].strip()
-        ## case 1: page numbers are divided by space from the next word. Example: 100-110 word
-        pos = re.search("\s",page_numbers) ## returns tuple with start and end position of a single space in the string 
-        if (pos != None):
-            pos = pos.span()
-            page_numbers = page_numbers[0:pos[0]-1].strip()
+def getYear(input):
+    pos = re.search("\d\d\d\d",input) ## check for 4 digits directly after earch other
+    counter = 0
+    while (pos != None):
+        if counter == 0:
+            output = input[pos.span()[0]:pos.span()[1]]
         else:
-        ## case 2: page numbers are divided by a comma from the next word. Example: 100-110, word
-            pos = re.search(",",page_numbers) ## returns tuple with start and end position of a comma in the string 
-            if (pos != None):
-                page_numbers = page_numbers[0:pos[0]-1].strip()
-            else:
-                page_numbers = None
-        return page_numbers
+            output = new_part[pos.span()[0]:pos.span()[1]]
+
+ 
+        if int(output) in range(1900,2022):
+            output = output
+            break
+        else:
+            new_index = re.search(f"{output}([\d\D]*)", input)
+            new_part = new_index.group(1)
+            pos = re.search("\d\d\d\d",new_part)
+        counter += 1
+
+    return output
+
+def getMonth(input):
+
+    pos = re.search("(?i)january|february|march|april|may|june|july|august|september|october|november|december|januari|februari|maart|mei|juni|juli|augustus|oktober",input)
+    if (pos != None):
+        output = input[pos.span()[0]:pos.span()[1]]
+    else:
+        output = None
+    return output
 
 def getXP(input):
-    ## hoe verbeteren: kijken of de output wel 9 cijfers is
-    ## cijfercheck toegevoegd
-
     pos = re.search('(?i)xp',input) 
     if (pos == None):
         return None
@@ -69,7 +73,6 @@ def getXP(input):
                 return None  
         return output
 
-
 def getISSN(input):
     ## hoe verbeteren: issn kan andere vormen aannemen dat alleen maar 8 getallen of er kunnen interpuncties in zitten
 
@@ -89,7 +92,7 @@ def getISSN(input):
             return output
 
 
-def getISBN(input): ##is deze nodig??
+def getISBN(input):  
     ## hoe verbeteren: isbn kan andere vormen aannemen dat alleen maar 8 getallen of er kunnen interpuncties in zitten
 
     pos = re.search('(?i)isbn',input) ## returns tuple with start and end position of ISBN in the string
@@ -107,6 +110,39 @@ def getISBN(input): ##is deze nodig??
             output = output[pos[0]:pos[0] + 13] ## isbn number is 13 characters
             return output
 
+
+
+def getPages(input):
+    ## hoe verbeteren: pagina in anderet talen zoeken of afkorting van pages, meer cases toevoegen
+
+    pos = re.search('(?i)pages|bladzijde|seite|page|pp\.', input) ## returns tuple with start and end position of pages in the string 
+    if (pos == None):
+        return None
+    else:
+        pos = pos.span() 
+        page_numbers = input[pos[1]+1:].strip()
+        ## case 1: page numbers are divided by space from the next word. Example: 100-110 word
+        pos = re.search("\s",page_numbers) ## returns tuple with start and end position of a single space in the string 
+        if (pos != None):
+            pos = pos.span()
+            page_numbers = page_numbers[0:pos[0]].strip()
+        pos = re.search(",",page_numbers) ## returns tuple with start and end position of a comma in the string 
+        elif pos:
+        ## case 2: page numbers are divided by a comma from the next word. Example: 100-110, word
+
+            if (pos != None):
+                page_numbers = page_numbers[0:pos[0]-1].strip()
+            else:
+                page_numbers = None
+        return page_numbers
+
+
+
+
+
+
+
+
 def getVol(input):
 
     pos = re.search('(?i)vol|deel',input)
@@ -119,10 +155,18 @@ def getVol(input):
         if (pos != None):
             output = output[pos.span()[0]:]
 
-            ## case 1, look for single space as end of volume number
-            ## case 2, look for comma as end of volume number
+            ## case 1, split volume based on issue in () behind it
+            ## case 2, look for single space as end of volume number
+            ## case 3, look for comma as end of volume number
 
-            ##first check which case is applicable
+            ##first check case 1
+            posbracket = re.search('\(', output)
+            if posbracket != None:
+                posbracket = posbracket.span()
+                return output[0:posbracket[0]]
+            
+            
+
             poswhitespace = re.search('\s',output)
             poscomma = re.search(',',output)
             if (poswhitespace != None and poscomma != None):
@@ -147,7 +191,7 @@ def getVol(input):
 
 def getIssue(input):
     # extra 'functie' toegevoegd om te kijken naar getallen tussen haakjes
-    pos = re.search('(?i)no.',input)
+    pos = re.search('(?i)no\.',input)
     if (pos != None):
         pos = pos.span()
         output = input[pos[1]:].strip()
@@ -181,18 +225,23 @@ def getIssue(input):
                 output = output[0:poscomma[0]]
             else:
                 output = None
-    check = input[input.find("(")+1:input.find(")")]
-    if check != '':
-        check_1 = int(input.index(")")-1)
-        check_2 = int(input.index("(")+1)
-        if check_1 - check_2 < 2 and check.isnumeric():
-            output = check
-        else:
-            output = getIssue(input[check_1 + 2:])
-        
         return output
-    else:
+    first_bracket = input.find("(")+1
+    second_bracket = input.find(")")
+    if second_bracket < 0:
         output = None
+    else:
+        check = input[input.find("(")+1:input.find(")")]
+        if check != '':
+            check_1 = int(input.index(")")-1)
+            check_2 = int(input.index("(")+1)
+            if check_1 - check_2 < 2 and check.isnumeric():
+                output = check
+                return output
+            else:
+                output = getIssue(input[check_1 + 2:])
+        else:
+            output = None
     return output
 
 def getAuthor(input):
@@ -200,6 +249,7 @@ def getAuthor(input):
     pos = re.search('(?i)et al|:',input)
     if (pos != None):
         output = input[0:pos.span()[0]].strip()
+        output = output.lower()
     else:
         output = None
 
@@ -214,7 +264,10 @@ def getAuthor(input):
         if further_splitted[0].isalpha():
             if len(i) == 1:
                 continue
+            elif re.search(r'\d', i):
+                continue
             output = i
+            output = output.lower()
             break
         else:
             output = pos_splitted
@@ -233,70 +286,32 @@ def getTitle(input):
             if (output[0]=="'"):
                 pos = re.search("'",output[1:])
                 output = output[1:pos.span()[1]]
+                output = output.lower()
             else:
                 pos = re.search(',',output)
                 if (pos != None):
                     output = output[0:pos.span()[1]-1]
+                    output = output.lower()
                 else:
                     output = None
     else:
         output = None
     return output
 
-def getYear(input):
-    pages = getPages(input)
-    pos = re.search("\d\d\d\d",input) ## check for 4 digits directly after earch other
-    if (pos != None):
-        output = input[pos.span()[0]:pos.span()[1]]
-        ## check for false positive, for example we took page numbers instead of year number
-        if (pages != None):
-            if (output in pages):
-                pos = re.search(pages,input).span()
-                output = input[pos[1]:]
-                pos = re.search('\d\d\d\d',output)
-                if (pos != None):
-                    output = output[pos.span()[0]:pos.span()[1]]
-                else:
-                    output = None
 
-    else:
-        output = None
-    return output
 
-def getMonth(input):
-    ## hoe verbeteren: zoek ook op maanden waar een type fout in staat
 
-    pos = re.search("(?i)january|february|march|april|may|june|july|augustus|september|october|november|december|januari|februari|maart|mei|juni|juli|oktober",input)
-    if (pos != None):
-        output = input[pos.span()[0]:pos.span()[1]]
-    else:
-        output = None
-    return output
 
-def getScore(validator,to_match):
-    score = 0
-    threshold = 3
-    ## increase score if validator metadata is the same as the to_match metadata
-    for metadata in validator.iteritems():
-        ## dont compare the cluster_id column
-        if metadata[0] == 'cluster_id':
-            continue
-        ## dont compare metadata if one of the two is none
-        elif metadata[1] != None:
-            if to_match[metadata[0]] != None:
-                ## if metadata is similar, increment score with 1
-                if str(metadata[1]) in str(to_match[metadata[0]]):
-                    score += 1
-                else:
-                    pass
-            else:
-                continue
-        else:
-            continue
-    ## return matching score
-    if score > threshold:
-        return True
-    else:
-        return False
 
-##print(getMonth("Daan van Turnhout et al, titel van dit boek is bla bla bal, May  .bladzijde: 1000-1100 asdfasdf 2022 asdf xp-123456789 adf ISSN:98475645 asdfasdf       ISBN:1234567891012 sdfsdf asdf vol  1011, sdfsd"))
+
+input = "Altschul S. F. et al. Gapped BLAST and PSI-BLAST: A new generation of protein database search programs Nucleic Acids Research. 1997, vol. 25(17), pages 3389-3402"
+print(getPages(input))
+print(getXP(input))
+print(getISSN(input))
+print(getISBN(input))
+print(getVol(input))
+print(getIssue(input))
+print(getAuthor(input))
+#print(getTitle(input))
+print(getYear(input))
+print(getMonth(input))
